36c460e3726611f8119513efa76d9545e2152f57
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-05 08:49:01 -0500

    feat: :sparkles: configuracion .env y servidor

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app=express();
 
-app.listen(3000, ()=>{
+app.use(express.json());
+
+const config=JSON.parse(process.env.MY_CONFIG);
+
+app.listen(config, ()=>{
     console.log("server corriendoo");
 })
